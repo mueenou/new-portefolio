47b194f5679ef6d9a79436b5428f837f7ad1b700
mhossain <m.hossain@blackcover.fr>
2020-02-23 05:33:46 +0100

    feat: basic css first commit #5

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,5 @@
 <template>
-    <div id="skills">
-        <h1>Skills</h1>
+    <div id="skills" class="wrapper">
     </div>
 </template>
 <script>
@@ -9,5 +8,50 @@ export default {
 }
 </script>
 <style scoped>
+html {
+  position: relative;
+  overflow-x: hidden !important;
+}
 
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Quicksand", sans-serif;
+  color: #324e63;
+}
+
+a,
+a:hover {
+  text-decoration: none;
+}
+
+.icon {
+  display: inline-block;
+  width: 1em;
+  height: 1em;
+  stroke-width: 0;
+  stroke: currentColor;
+  fill: currentColor;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 1230px !important;
+  width: 100%;
+  height: auto;
+  text-align: center;
+}
+@media screen and (max-width: 768px) {
+  .wrapper {
+    height: auto;
+    min-height: 100vh;
+    padding-top: 100px;
+  }
+}
 </style>
